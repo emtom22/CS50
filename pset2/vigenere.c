0585a5656721a4c69b2641b14216b0e59d67b7cd
emtom22 <emtom22@github.com>
2019-05-10 13:38:07 -0700

    add vigenere encryption definition

---
+++ pset2/vigenere.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+/*
+The Vigenere encryption uses a keyphrase to encrypt messages. 
+For example, if the keyword is "dog" the shift would be
+4 (d) -> 14 (o) -> 6 (g)
+So if message was "hello world" it would encrypt by:
+shift h by 3 (d)
+shift e by 14 (o)
+shift l by 6 (g)
+shift l by 3 (d)
+etc...
+*/
+
+int main(int argc, string argv[])
+{
+
+}
