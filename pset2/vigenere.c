12fbd68bd00a0a29bd24537f87cbc4da0aa7fa36
emtom22 <emtom22@github.com>
2019-05-10 13:44:20 -0700

    added problem statement to comments

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -11,6 +11,19 @@ shift e by 14 (o)
 shift l by 6 (g)
 shift l by 3 (d)
 etc...
+
+A is a shift of 0
+Z is a shift of 25
+
+At execution of program the user will type a keyword to use for encryption
+(i.e. /vigenere.c testkey)
+	> If user does not input keyword OR 
+		inputs non-alpha characters (including spaces, digits, symbols)
+	  	end the execution with message "Usage ./vigenere keyword"
+
+After execution, prompt user for plaintext "plaintext: "
+Then program will use the keyword to encrypt the message and display the
+message on the next line as "ciphertext: ___________"
 */
 
 int main(int argc, string argv[])
