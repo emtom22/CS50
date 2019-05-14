57bc05ee6d2e40bc450402b825703f62e45ee517
emtom22 <emtom22@github.com>
2019-05-13 21:50:13 -0700

    added first draft of vigenere2.c logic encrypt function is not working as expected

--- pset2/vigenere2.c
+++ pset2/vigenere2.c
@@ -1,5 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 /*
 The Vigenere encryption uses a keyphrase to encrypt messages. 
@@ -25,26 +27,29 @@ After execution, prompt user for plaintext "plaintext: "
 Then program will use the keyword to encrypt the message and display the
 message on the next line as "ciphertext: ___________"
 */
-
-bool isAlpha(string str, int str_length);
-char rotateChar(char text, char key);
-int encrypt(string text, string keyword);
+char rotateChar(char ch, char key);
+int encrypt(string text, string key, int text_len, int key_len);
 
 int main(int argc, string argv[])
 {
 	// Ensure user inputs only one encryption keyword during execution
-	if(argc != 2){
+	if(argc != 2)
+	{
 		printf("Usage ./vigenere keyword\n");
 		return 1;
 	}
-	int keyword = argv[1];
+	string keyword = argv[1];
 	int keyword_len = strlen(keyword);
 	char current_key = keyword[0];
 
-	// Ensure keyword is alphabetic
-	if(!isAlpha(keyword, strlen(keyword))){
-		printf("Usage ./vigenere keyword\n");
-		return 1;
+	// Ensure keyword is all alpha
+	for(int i = 0; i < keyword_len; i++)
+	{
+		if(!isalpha(keyword[i]))
+		{
+			printf("Usage ./vigenere keyword\n");
+			return 1;
+		}
 	}
 
 	// Ask user for message to encrypt
@@ -52,57 +57,50 @@ int main(int argc, string argv[])
 	int text_len = strlen(plaintext);
 	
 	// Copy text into ciphertext to iterate through
-	char ciphertext[text_len];
+	// char ciphertext[text_len];
+	string ciphertext;
 	strcpy(ciphertext, plaintext);
 
 	// Iterate over entire text that needs encrypting
-	encrypt(cipertext);
+	encrypt(ciphertext, keyword, text_len, keyword_len);
 	printf("Ciphertext is: %s\n", ciphertext);
-
 }
 
 
-// Function to check if string is all alpha characters
-bool isAlpha(string str, int str_length){
-
-	// Copy string into array of char
-	// Iterate through string to find if all char are alpha
-	for(int i = 0; i < str_length; i++){
-		printf("%c", str[i]);
-		if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')){
-			return false;
-		}
-	}
-	return true;
-}
-
-
+// Only rotates alpha characters. If non-alpha, return inputted char
 char rotateChar(char ch, char key){
 	
-	// Define orignal case of char
-	bool is_uppercase = false;
-	if(charactte >= 'A' && ch <= 'Z'){
-		is_uppercase = true;
+	// Return char if non-alpha
+	if(!isalpha(ch)){
+		return ch;
 	}
 
-	// Rotate char
-	char rotated_ch = upper(ch) + key - 'A';
+	// Rotate char by key where...
+	// A = 1 rotation up (i.e. A becomes B)
+	// B = 2 rotations up (i.e. A becomes C)
+	char rotated_ch = toupper(ch) + key - 'A';
 
 	// Return the original case of the character
-	if(is_uppercase){
+	if(isupper(ch))
+	{
 		return rotated_ch;
 	}
-	return lower(rotated_ch);
-
+	return tolower(rotated_ch);
 }
-	// Encrypt message}
+
+// Encrypt message
 int encrypt(string text, string key, int text_len, int key_len){
 	// can i modify string directly? or do i have to copy and make an array?
 	// i should be able to modify directly
 	for(int i = 0, j = 0; i != '\0'; i++, j++)
+	{
 		rotateChar(text[i], key[j]);
-		// Ensure key is coninually rotated through
-		if (j == key_len){
+
+		// Keyword retuns to first letter of encryption when get to end of keyword
+		if (j == key_len)
+		{
 			j = 0;
 		}
+	}
+	return 0;
 }
