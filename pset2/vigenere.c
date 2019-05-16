a19f5dd0d88750456cc5ca07c74ea4579a9cde38
emtom22 <emtom22@github.com>
2019-05-16 08:12:11 -0700

    finished vigenere problem fixed the rotation of char issue

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -1,5 +1,7 @@
-#include <cs50.h>
+#include </home/etgonehomie/projects/cs50/libcs50-9.0.1/src/cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 /*
 The Vigenere encryption uses a keyphrase to encrypt messages. 
@@ -25,84 +27,94 @@ After execution, prompt user for plaintext "plaintext: "
 Then program will use the keyword to encrypt the message and display the
 message on the next line as "ciphertext: ___________"
 */
-
-bool isAlpha(string str, int str_length);
-char rotateChar(char text, char key);
-int encrypt(string text, string keyword);
-
+char rotateChar(char ch, char key);
+int encrypt(string text, string key, int text_len, int key_len);
 int main(int argc, string argv[])
-{
+{	
+	
 	// Ensure user inputs only one encryption keyword during execution
-	if(argc != 2){
+	if(argc != 2)
+	{
 		printf("Usage ./vigenere keyword\n");
 		return 1;
 	}
-	int keyword = argv[1];
+	string keyword = argv[1];
 	int keyword_len = strlen(keyword);
-	char current_key = keyword[0];
 
-	// Ensure keyword is alphabetic
-	if(!isAlpha(keyword, strlen(keyword))){
-		printf("Usage ./vigenere keyword\n");
-		return 1;
+	// Ensure keyword is all alpha
+	for(int i = 0; i < keyword_len; i++)
+	{
+		if(!isalpha(keyword[i]))
+		{
+			printf("Usage ./vigenere keyword\n");
+			return 1;
+		}
 	}
-
+	
 	// Ask user for message to encrypt
 	string plaintext = get_string("Input text to encrypt: ");
 	int text_len = strlen(plaintext);
-	
+
 	// Copy text into ciphertext to iterate through
-	char ciphertext[text_len];
-	strcpy(ciphertext, plaintext);
+	string ciphertext = plaintext;
 
 	// Iterate over entire text that needs encrypting
-	encrypt(cipertext);
+	encrypt(ciphertext, keyword, text_len, keyword_len);
 	printf("Ciphertext is: %s\n", ciphertext);
-
 }
 
 
-// Function to check if string is all alpha characters
-bool isAlpha(string str, int str_length){
-
-	// Copy string into array of char
-	// Iterate through string to find if all char are alpha
-	for(int i = 0; i < str_length; i++){
-		printf("%c", str[i]);
-		if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')){
-			return false;
-		}
-	}
-	return true;
-}
-
-
+// Only rotates alpha characters. If non-alpha, return inputted char
 char rotateChar(char ch, char key){
-	
-	// Define orignal case of char
-	bool is_uppercase = false;
-	if(charactte >= 'A' && ch <= 'Z'){
-		is_uppercase = true;
+	int num_of_letters = 26;
+	// int normalize_key = toupper(key) - 'A';
+	// Return char if non-alpha
+	if(!isalpha(ch)){
+		return ch;
 	}
 
-	// Rotate char
-	char rotated_ch = upper(ch) + key - 'A';
+	// Rotate char by key where...
+	// A = 0 rotation up (i.e. Z becomes Z)
+	// B = 1 rotations up (i.e. Z becomes A)
+	// printf("upper ch: %i %c\n", toupper(ch), toupper(ch));
+	// printf("upper key: %i %c\n", toupper(key), toupper(key));
+
+
+	char rotated_ch = ((toupper(ch) - 'A' + toupper(key) - 'A') % 26) + 'A';
+
+	// printf("ch: %c : %c\n", ch, rotated_ch);
 
 	// Return the original case of the character
-	if(is_uppercase){
+	if(isupper(ch))
+	{
 		return rotated_ch;
 	}
-	return lower(rotated_ch);
-
+	
+	return tolower(rotated_ch);
 }
-	// Encrypt message}
+
+// Encrypt message
 int encrypt(string text, string key, int text_len, int key_len){
 	// can i modify string directly? or do i have to copy and make an array?
 	// i should be able to modify directly
-	for(int i = 0, j = 0; i != '\0'; i++, j++)
-		rotateChar(text[i], key[j]);
-		// Ensure key is coninually rotated through
-		if (j == key_len){
+	// printf("plaintext: %s\n", text);
+	// printf("key: %s\n", key);
+	for(int i = 0, j = 0; i < text_len; i++)
+	{
+		// Only rotate through key if alpha character
+		if(isalpha(text[i])){
+			// printf("j beginning of loop is = %i\n", j); 	
+			text[i] = rotateChar(text[i], key[j]);
+			j++;
+		}
+
+		
+		// Keyword retuns to first letter of encryption when get to end of keyword
+		if (j == key_len)
+		{
 			j = 0;
 		}
+		// printf("j end of loop is = %i\n", j);
+	}
+	return 0;
 }
