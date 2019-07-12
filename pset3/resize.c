0f295b2f99d786b9bd5e816fa0e5ae98d6184f37
emtom22 <emtom22@github.com>
2019-07-11 18:23:56 -0700

    finished resize int and copied into resize.c

--- pset3/resize.c
+++ pset3/resize.c
@@ -24,70 +24,34 @@ Your program, if it uses malloc, must not leak any memory.
 
 #include "bmp.h"
 
-float round_f(float f, int sig_digit);
-bool isFloat(char* f);
-
-int TENTH = 10;
-int HUNDRETH = 100;
+int strtoi(char* i);
 
 int main(int argc, char *argv[])
 {
-    // ensure proper usage
+    // Ensure proper usage of calling the program
     if (argc != 4)
     {
-        printf("Usage: ./resize f infile outfile\n");
+        printf("Error 1 -> Usage: ./resize i infile outfile\n");
         return 1;
     }
 
-    
-    // Define the inputted float
-    char* temp_f = argv[1];
-    float resize = 0;
-    
-    // Defines full int value and each decimal digit of the inputted float
-    // This enables logic to know how many dupes to make per pixel
-    // and what pixels to dupe.
-    int resize_full_int = 0;
-    int resize_tenth_decimal = 0;
-    int resize_hund_decimal = 0;
-
-    // Defines what pixels to dupe based on the inputted resize value.
-    // Tells what pixel to dupe based on the 10th decimal and 100th decimal.
-    int dupe_tenth = 0;
-    int dupe_hund = 0;
-
-    // Variables to define what pixel in a row or column I am in
-    // This will help know what 10th and 100th place pixels to dupe
-    int pixel_row = 0;     
-    int pixel_col = 0; 
-
-    // Ensure 2md variable is a float
-    if(!is_float(temp_f)){
-        printf("Must input a valid float\n");
-        return 1;
-    }
-    else{
-        resize = strtof(temp_f, NULL);
-        resize = round_f(resize, 2);
-        resize_full_int = round(resize); //ensure this just rounds down all the time
-        resize_tenth_decimal = round((resize - resize_full_int) * TENTH);
-        resize_hund_decimal = round((resize - resize_full_int) * HUNDRETH) - (resize_tenth_decimal * TENTH);
-
-        // Define which pixels need to be duped for the tenth and hundreth places
-        // ensure I handle 0s good
-        dupe_tenth = TENTH / resize_tenth_decimal;
-        dupe_hund = HUNDRETH / resize_hund_decimal;
-    }
-
-    // remember filenames
+    // Store the inputted parameters
+    int resize = 0;
     char *infile = argv[2];
     char *outfile = argv[3];
 
+    // Ensure 2nd variable is an int > 0
+    resize = strtoi(argv[1]);
+    if (resize < 1) {
+        printf("Error 3 -> Must input a valid resize int greater than 0\n");
+        return 3;
+    }
+    
     // open input file
     FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
-        printf("Could not open %s.\n", infile);
+        printf("Error 3 -> Could not open %s.\n", infile);
         return 2;
     }
 
@@ -117,73 +81,79 @@ int main(int argc, char *argv[])
         printf("Unsupported file format.\n");
         return 4;
     }
+		
+	// Create new BITMAP headers
+	BITMAPFILEHEADER bf_new = bf; // Does this copy or just create a pointer to the same?
+	BITMAPINFOHEADER bi_new = bi; // Does this copy or just create a pointer to the same?
 
+	// Resize file info variables
+	bi_new.biWidth = bi.biWidth * resize;
+	bi_new.biHeight = bi.biHeight * resize;
+	
+	// Determine if any padding needed for width
+	int padding_old = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    int padding_new = (4 - (bi_new.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+	
+	bi_new.biSizeImage = abs((bi_new.biWidth * sizeof(RGBTRIPLE) + padding_new) * bi_new.biHeight);
+		
+	// Resize file header variables
+	bf_new.bfSize = sizeof(bf_new) + sizeof(bi_new) + bi_new.biSizeImage;
+	
     // write outfile's BITMAPFILEHEADER
-    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+    fwrite(&bf_new, sizeof(BITMAPFILEHEADER), 1, outptr);
 
     // write outfile's BITMAPINFOHEADER
-
-    // modify outfile bitmap info header
-    // width = round(bi.biWidth * resize + 0.5)
-    // bi.biHeight  = round(bi.biHeight * resize + 0.5)
-    // size = bi.biHeight * bi.biWidth
-
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
-
-
-    // determine padding for scanlines
-    // padding will change according to the resize.
-    /*
-    say width is 10 pixels
-    that would be a padding of 2 (4 + 4 + 2 with 2 padding since its group of 4)
-    say multiple width by 2 (20 % 4 = 0, so 0 padding)
-    say multiply width by 2.25 (22.5), round this up to 23 (23 % 4 = 1, so 1 padding)
-
-    */
-    // padding:
-    // say it is 10 pixels
-    
-
-    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    fwrite(&bi_new, sizeof(BITMAPINFOHEADER), 1, outptr);
 
     // START RESIZING HERE
-    /**
-     * for every digit do "decimal place (ie 10, 100, 1000, / digit"
-     * 
-     * for 2.49 it would be
-     * 10/4 = 2 --> dupe every 2nd digit then when count is 4, skip to 10 and start at
-     *              1 again
-     * 100/9 = 11  --> dupe every 11th digit, then when it hits 100 and start at 1 again
-     *  
-     * 
-     * **/
-    // TO-DO
 
+
+    // initialize how many times each pixel and row has been duplicated
+	int num_pixel_dupes = 0;
+	int num_row_dupes = 0;
+    
     // iterate over infile's scanlines
-    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
-    {
-        // iterate over pixels in scanline
-        for (int j = 0; j < bi.biWidth; j++)
-        {
-            // temporary storage
-            RGBTRIPLE triple;
+	for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+	{
 
-            // read RGB triple from infile
-            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+        num_row_dupes = 0;
+        // Write entire row to outfile resized times
+        do {
+			// iterate over pixels in scanline
+			for (int j = 0; j < bi.biWidth; j++)
+			{
+				// temporary storage
+				RGBTRIPLE triple;
 
-            // write RGB triple to outfile
-            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
-        }
+				// read RGB triple from infile
+				fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
-        // skip over padding, if any
-        fseek(inptr, padding, SEEK_CUR);
+				// write RGB triple to outfile resized times
+				
+                num_pixel_dupes = 0;
+				do {
+					fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+		
+					num_pixel_dupes++;
+				} while (resize > num_pixel_dupes);
+			}
 
-        // then add it back (to demonstrate how)
-        for (int k = 0; k < padding; k++)
-        {
-            fputc(0x00, outptr);
-        }
-    }
+			// skip over padding, if any
+			fseek(inptr, padding_old, SEEK_CUR);
+
+			// then add it back (to demonstrate how)
+			for (int k = 0; k < padding_new; k++)
+			{
+				fputc(0x00, outptr);
+			}
+			
+			// Reset pointer to beginning of row if still need to resize
+			num_row_dupes++;
+			if (resize > num_row_dupes) {
+				fseek(inptr, -bi.biWidth*3, SEEK_CUR);
+			}
+		} while (resize > num_row_dupes);
+	}
 
     // close infile
     fclose(inptr);
@@ -195,34 +165,14 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-bool isFloat(char* f){
-    if (strlen(f) > 0 && !isspace((unsigned char) f[0])){
-        char *tail;
-        errno = 0;
-        float f = strtof(f, &tail);
-        if (errno == 0 && *tail == '\0' && isfinite(f) != 0 && f < FLT_MAX)
-        {
-            // Disallow hexadecimal and exponents
-            if (strcspn(f, "XxEePp") == strlen(f))
-            {
-                return true;
-            }
-        }
-    }
-}
-
-// Round a float 'f' to the significant digit 'sig_digit'
-// 2 means round to the 100th place (i.e. 12.345 becomes 12.35)
-// can only round to the 5th digit, else it will return same float passed
-float round_f(float f, int sig_digit){
-    int max_sig_digit = 5;
+int strtoi(char* i){
     
-    // Only allow rounding to max of 5 significant digits
-    if (sig_digit > max_sig_digit){
-        return f;
-    }
-
-    float multiplier = pow(10.0, sig_digit);
-    int temp_f = round(f * multiplier);
-    return temp_f / multiplier;
+     // Return an true if only a valid int was provided
+    char *tail;
+    errno = 0;
+    long n = strtol(i, &tail, 10);
+    if (errno == 0 && *tail == '\0' && n >= INT_MIN && n < INT_MAX)
+        return n;
+    
+    return -1;
 }
