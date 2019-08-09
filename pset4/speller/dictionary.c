9614c6f6c816e8d0f78b2c873e77a6a9c02a2df5
emtom22 <emtom22@github.com>
2019-08-08 21:49:28 -0700

    tested and upd hash function to hash by first 2 digits

--- pset4/speller/dictionary.c
+++ pset4/speller/dictionary.c
@@ -8,7 +8,9 @@
 #include "dictionary.h"
 
 // Represents number of buckets in a hash table
-#define N 26
+// #define N 26
+
+#define N 703 // 26 first letter * 27 second letter + 1
 
 // Represents a node in a hash table
 typedef struct node
@@ -21,10 +23,28 @@ node;
 // Represents a hash table
 node *hashtable[N];
 
-// Hashes word to a number between 0 and 25, inclusive, based on its first letter
+// Hashes word to a number between 0 and 703, inclusive, based on combinations of first 2 letters 
+// This assumes that 1st is alpha and 2nd is alpha or apostrophe
+// also add an extra bucket for 1 letter words
+// 25 first letters * 26 second letters with apostrophe + 1 bucket for single char words
 unsigned int hash(const char *word)
 {
-    return tolower(word[0]) - 'a';
+    int digit_1 = tolower(word[0]) - 'a';
+    int digit_2 = 0;
+
+    // Check if 2nd digit is non-alpha or non-existent
+    // If only 1 digit, then it goes in last bucket
+    if (word[1] == '\''){
+        digit_2 = 26;
+    }
+    else if (word[1] == '\0'){
+        return 703;
+    }
+    else {
+        digit_2 = tolower(word[1] - 'a');
+    }
+
+    return (digit_1 * 26) + (digit_2 + digit_1);
 }
 
 // Loads dictionary into memory, returning true if successful else false
@@ -51,6 +71,16 @@ bool load(const char *dictionary)
     while (fscanf(file, "%s", word) != EOF)
     {
         // TODO
+        hash(word);
+
+        // if new,then add a new header
+        // if not then, add to existing linked list
+
+        // It is assumed that all dictionaries loaded are alphabetized and lower case
+        // Therefore, will do a binary search to sort into hash tables.
+
+
+        // REMEMBER THAT DICTIONARY IS SORTED ALPHABETICALLY
     }
 
     // Close dictionary
